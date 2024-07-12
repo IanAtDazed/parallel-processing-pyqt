3e10982f5769b4433390fd1e13f680b2ab962ecd
IanAtDazed <ian.robert.cunningham@gmail.com>
2024-07-12 13:26:14 -0700

    imports ParallelView

--- main.py
+++ main.py
@@ -1,34 +1,8 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel, QPushButton
-
-class MainWindow(QMainWindow):
-    """A single window application to demonstrate parallel processing with PyQt6."""
-
-    def __init__(self):
-        """MainWindow constructor."""
-        super().__init__()
-
-        self._layout_widgets()
-
-    def _layout_widgets(self) -> None:
-        """Layout the PyQt widgets."""
-
-        outer_layout = QVBoxLayout()
-
-        self.start_button = QPushButton('Start')
-        self.stop_button = QPushButton('Stop')
-        self.result_label = QLabel('N/a')
-
-        outer_layout.addWidget(self.start_button)
-        outer_layout.addWidget(self.stop_button)
-        outer_layout.addWidget(self.result_label)
-
-        widget = QWidget()
-        widget.setLayout(outer_layout)
-
-        self.setCentralWidget(widget)
+from PyQt6.QtWidgets import QApplication
+from view.parallel_view import ParallelView
 
 if __name__ == '__main__':
     app = QApplication([])
-    window = MainWindow()
-    window.show()
+    view = ParallelView()
+    view.show()
     app.exec()
