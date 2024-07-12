df4a5e4b0eeb2d70b3223cac163a38f88d0ac91f
IanAtDazed <ian.robert.cunningham@gmail.com>
2024-07-12 14:41:44 -0700

    _process_signal implemented

--- view/parallel_view.py
+++ view/parallel_view.py
@@ -7,7 +7,7 @@ if TYPE_CHECKING:
 
 from PyQt6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QLabel, QPushButton
 
-
+from helpers.named_tuples import MessageSignal
 class ParallelView(QMainWindow):
     """A single window application to demonstrate parallel processing with PyQt6."""
 
@@ -42,3 +42,14 @@ class ParallelView(QMainWindow):
 
         self.start_button.clicked.connect(self._viewmodel.start_button_clicked)
         self.stop_button.clicked.connect(self._viewmodel.stop_button_clicked)
+        self._viewmodel.signal.connect(self._process_signal)
+    
+    def _process_signal(self, signal: MessageSignal) -> None:
+        """Process a signal from the viewmodel.
+        
+        Args:
+            signal: The signal to process.
+        """
+
+        print('_process_signal')
+        print(signal)
