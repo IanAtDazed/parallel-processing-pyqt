5cdf560fde4aa72c4eb5550d28df990a7e697fff
IanAtDazed <ian.robert.cunningham@gmail.com>
2024-07-12 14:13:58 -0700

    viewmodel instantiated

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+"""main module for the application.
+
+Fires up the *ParallelView* *QMainWindow*
+"""
+
 from PyQt6.QtWidgets import QApplication
+
+from model.parallel_model import ParallelModel
+from viewmodel.parallel_view_model import ParallelViewModel
 from view.parallel_view import ParallelView
 
 if __name__ == '__main__':
     app = QApplication([])
-    view = ParallelView()
+    model = ParallelModel()
+    viewmodel = ParallelViewModel(model)
+    view = ParallelView(viewmodel)
     view.show()
     app.exec()
