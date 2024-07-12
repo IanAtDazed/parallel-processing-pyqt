746e961769f56dae5bc710e1c95829c57f50e48d
IanAtDazed <ian.robert.cunningham@gmail.com>
2024-07-12 14:16:08 -0700

    _connect_signals_to_slots implemented

--- view/parallel_view.py
+++ view/parallel_view.py
@@ -1,13 +1,22 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from viewmodel.parallel_view_model import ParallelViewModel
+
+
 from PyQt6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QLabel, QPushButton
 
 class ParallelView(QMainWindow):
     """A single window application to demonstrate parallel processing with PyQt6."""
 
-    def __init__(self):
+    def __init__(self, viewmodel: ParallelViewModel) -> None:
         """MainWindow constructor."""
         super().__init__()
 
+        self._viewmodel = viewmodel
         self._layout_widgets()
+        self._connect_signals_to_slots()
 
     def _layout_widgets(self) -> None:
         """Layout the PyQt widgets."""
@@ -26,3 +35,10 @@ class ParallelView(QMainWindow):
         widget.setLayout(outer_layout)
 
         self.setCentralWidget(widget)
+    
+    def _connect_signals_to_slots(self) -> None:
+        """Connect signals to slots."""
+
+        self.start_button.clicked.connect(self._viewmodel.start_button_clicked)
+        self.stop_button.clicked.connect(self._viewmodel.stop_button_clicked)
+
