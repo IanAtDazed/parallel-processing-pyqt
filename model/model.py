614ae7c9f993e10e4a7f1e40f0ca9fe18220518c
IanAtDazed <ian.robert.cunningham@gmail.com>
2024-07-12 17:07:07 -0700

    stop implemented

--- model/model.py
+++ model/model.py
@@ -1,7 +1,34 @@
 """Model module."""
 
+from PyQt6.QtCore import QThread
+
+from parallel.parallel_worker import ParallelWorker
 
 class Model:
     """Model class."""
 
-    pass
+    def __init__(self) -> None:
+        """Initialize the model."""
+
+        self._thread = QThread()
+        self._worker = ParallelWorker(self._callback_function)
+        self._worker.moveToThread(self._thread)
+        self._thread.started.connect(self._worker.start)
+
+    def run(self) -> None:
+        """Run the model."""
+
+        self._thread.start()
+    
+    def stop(self) -> None:
+        """Stop the model."""
+
+        self._worker.quit()
+        # TODO: Need both?
+        self._thread.quit()
+        self._thread.wait()
+    
+    def _callback_function(self, value: any) -> None:
+        """Callback function."""
+
+        print(f'Callback function: {value}')
