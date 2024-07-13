caceaa7b2188a5b325f5c0b1f01bb13d505421f7
IanAtDazed <ian.robert.cunningham@gmail.com>
2024-07-13 08:32:35 -0700

    ProcessStartedSignal, ProcessQuitSignal added

--- helpers/named_tuples.py
+++ helpers/named_tuples.py
@@ -4,6 +4,12 @@ from typing import NamedTuple
 
 
 class UpdateSignal(NamedTuple):
-    """Indicates a value is available for the view."""
+    """Indicates a new value is available for the view."""
 
     value: int
+
+class ProcessStartedSignal(NamedTuple):
+    """Indicates, to the view, the process has started."""
+
+class ProcessQuitSignal(NamedTuple):
+    """Indicates, to the view, the process has been quit."""
