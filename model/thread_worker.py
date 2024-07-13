e156d4264c0fb48f502cd3e5f37ff0c1382a6f59
IanAtDazed <ian.robert.cunningham@gmail.com>
2024-07-13 10:44:27 -0700

    _process_data method implemented

--- model/thread_worker.py
+++ model/thread_worker.py
@@ -4,13 +4,14 @@ import ray
 from PyQt6.QtCore import QObject, pyqtSignal
 from ray.util.queue import Queue
 
+from helpers.process_instruction_named_tuples import QuitProcessing
 from parallel.parallel_supervisor import ParallelSupervisor
 
 
 class ThreadWorker(QObject):
     """Class to run the parallel processing."""
 
-    result = pyqtSignal(int)  # TODO: is object suitable?
+    result = pyqtSignal(int)
 
     def __init__(self, callback_function: callable) -> None:
         """Initialzie the class."""
@@ -19,33 +20,38 @@ class ThreadWorker(QObject):
 
         self.result.connect(callback_function)
 
-        self._queue = Queue()
+        self._instruction_queue = Queue()
+        self._result_queue = Queue()
+        self._parallel_supervisor = None
 
         self._is_running = False
-
-        self._counter = 0  # TODO: TEMP
 
     def start(self) -> None:
         """Start the parallel processing."""
 
-        self._parallel_supervisor = ParallelSupervisor.remote(self._queue)
+        self._start_parallel_supervisor()
+        self._process_data()
+
+    def _start_parallel_supervisor(self) -> None:
+        """Run the parallel supervisor."""
+
+        self._parallel_supervisor = ParallelSupervisor.remote(
+            self._instruction_queue, self._result_queue)
         self._parallel_supervisor.start.remote()
 
         self._is_running = True
 
+    def _process_data(self) -> None:
+        """Process the data."""
+
         while self._is_running:
-            # TODO
-            # self.result.emit(self._counter)
-            # time.sleep(1)
-            # self._counter += 1
-            latest_result = self._queue.get()
-            print(latest_result) # TODO
+            latest_result = self._result_queue.get()
             self.result.emit(latest_result)
 
     def quit(self) -> None:
         """Quit the parallel processing."""
 
-        print('ThreadWorker quit') # TODO
+        print('ThreadWorker quit')  # TODO
 
+        self._instruction_queue.put(QuitProcessing())
         self._is_running = False
-        self._parallel_supervisor.quit.remote()
