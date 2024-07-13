0d45edb11c39660403d0d537de6151c480572c6a
IanAtDazed <ian.robert.cunningham@gmail.com>
2024-07-13 08:02:58 -0700

    Docstring added

--- view/view.py
+++ view/view.py
@@ -1,21 +1,22 @@
-from __future__ import annotations
-from typing import TYPE_CHECKING
+"""Module containing the application's main *view*
 
-if TYPE_CHECKING:
-    from viewmodel.viewmodel import Model
-
+- A *QMainWindow*
+"""
 
 from PyQt6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QLabel, QPushButton
 
+from viewmodel.viewmodel import ViewModel
 from helpers.named_tuples import MessageSignal
+
+
 class View(QMainWindow):
     """A single window application to demonstrate parallel processing with PyQt6."""
 
-    def __init__(self, viewmodel: Model) -> None:
+    def __init__(self) -> None:
         """MainWindow constructor."""
         super().__init__()
 
-        self._viewmodel = viewmodel
+        self._viewmodel = ViewModel()
         self._layout_widgets()
         self._connect_signals_to_slots()
 
@@ -43,10 +44,10 @@ class View(QMainWindow):
         self.start_button.clicked.connect(self._viewmodel.start_button_clicked)
         self.stop_button.clicked.connect(self._viewmodel.stop_button_clicked)
         self._viewmodel.signal.connect(self._process_signal)
-    
+
     def _process_signal(self, signal: MessageSignal) -> None:
         """Process a signal from the viewmodel.
-        
+
         Args:
             signal: The signal to process.
         """
