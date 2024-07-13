213f50fee0386c0c8d5a65c0ada25a7d5fd346d0
IanAtDazed <ian.robert.cunningham@gmail.com>
2024-07-13 07:41:44 -0700

    Model instantiation moved to ViewModel

--- main.py
+++ main.py
@@ -4,15 +4,13 @@ Fires up the *ParallelView* *QMainWindow*
 """
 
 from PyQt6.QtWidgets import QApplication
-
-from model.model import Model
 from viewmodel.viewmodel import ViewModel
 from view.view import View
 
 if __name__ == '__main__':
     app = QApplication([])
-    model = Model()
-    viewmodel = ViewModel(model)
+    
+    viewmodel = ViewModel()
     view = View(viewmodel)
     view.show()
     app.exec()
